8103d6fcfc6414f51c64dde8fc9de9e05ff58949
BrevinS <brevinsimon0@gmail.com>
2022-03-22 00:15:15 -0700

    Added database, models.py, forms.py

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField, IntegerField, PasswordField, SelectField, BooleanField
+from wtforms.validators import ValidationError, Length, DataRequired, Email, EqualTo
+from app.models import User
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password1 = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), 
+                EqualTo('password1')])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        _user = User.query.filter_by(username=username.data).first()
+        if _user is not None:
+            raise ValidationError('Username already exists! Try another...')
+            
+    def validate_email(self, email):
+        _user = User.query.filter_by(email=email.data).first()
+        if _user is not None:
+            raise ValidationError('Email has already been used! Try another...')
