53125054cd67588a6ef86e9092f8fa9c7292e64e
BrevinS <brevinsimon0@gmail.com>
2022-03-22 18:19:14 -0700

    added login page

--- app/routes.py
+++ app/routes.py
@@ -8,22 +8,14 @@ from flask_login import current_user, login_user, logout_user, login_required
 @app.before_first_request
 def initDB(*args, **kwargs):
     db.create_all()
-   
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return render_template('base.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        flash('You must logout to use this feature!')
-        return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
         acc = User(username=form.username.data, email=form.email.data, firstname=form.firstname.data,
-            lastname=form.lastname.data, phone=form.phone.data)
+            lastname=form.lastname.data)
         acc.get_password(form.password2.data)
         db.session.add(acc)
         db.session.commit()
@@ -31,4 +23,19 @@ def register():
         ##REDIRECT AFTER STUDENT REGISTERS THEY NEED TO SEE COURSES && APPLY
         ##I need to add new page
         return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('user_registration.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        acc = User(username=form.username.data, email=form.email.data, firstname=form.firstname.data,
+            lastname=form.lastname.data)
+        acc.get_password(form.password2.data)
+        db.session.add(acc)
+        db.session.commit()
+        flash('Congrats you have created an account!')
+        ##REDIRECT AFTER STUDENT REGISTERS THEY NEED TO SEE COURSES && APPLY
+        ##I need to add new page
+        return redirect(url_for('index'))
+    return render_template('user_registration.html', title='Register', form=form)
